8950ea163f23ca9c09d9c210f835195dde92ff28
joshnwosu <joshuanwosu078@gmail.com>
2023-02-04 13:35:34 +0100

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,35 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('title')</title>
+    <title>@yield('title')</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        {{-- Style --}}
-        @vite(['resources/css/app.css','resources/js/app.js'])
-    </head>
-    <body class="bg-gray-50 dark:bg-gray-900">
-        <div class="container">
-        @yield('content')
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    {{-- Style --}}
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<body class="bg-gray-50 dark:bg-gray-900">
 
-        @if (session()->has('success'))
-            <div class="alret alert-success">
-                {{session()->get('success')}}
-            </div>
-        @endif
+    @include('partials.navbar')
+
+    @if (session()->has('success'))
+    <div class="flex p-4 mb-4 text-sm text-green-800 bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+        <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
+        </svg>
+        <span class="sr-only">Info</span>
+        <div>
+            {{session()->get('success')}}
         </div>
-    </body>
+    </div>
+    @endif
+
+    @yield('content')
+
+
+    @include('partials.footer')
+</body>
 </html>
